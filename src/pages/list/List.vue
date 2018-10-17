d6a63c179452c97b1655e05b6ea8d754eddc4a2d
huangmeideng <huangmeideng@outlook.com>
2018-10-17 23:09:00 +0800

    test vue component

--- src/pages/list/List.vue
+++ src/pages/list/List.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    list
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'List'
 }
 </script>
 
 <style>
+
 </style>
