9231f957fa5cdf8f9271207aedf7089e8958d86f
huangmeideng <huangmeideng@outlook.com>
2018-10-21 19:37:23 +0800

    city-ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -4,126 +4,29 @@
       <div class="area">
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
-
           <div class="button-wrapper">
             <div class="button">
               南京
             </div>
           </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
         </div>
       </div>
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-
-          <div class="button-wrapper">
+          <div class="button-wrapper" v-for="item in hot" :key="item.id">
             <div class="button">
-              南京
+              {{item.name}}
             </div>
           </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
-          <div class="button-wrapper">
-            <div class="button">
-              南京
-            </div>
-          </div>
-
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) in cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom">AAAA</div>
-          <div class="item border-bottom">AAAA</div>
-          <div class="item border-bottom">AAAA</div>
-        </div>
-      </div>
-
-      <div class="area">
-        <div class="title border-topbottom">B</div>
-        <div class="item-list">
-          <div class="item border-bottom">BBBB</div>
-          <div class="item border-bottom">BBBB</div>
-          <div class="item border-bottom">BBBB</div>
-        </div>
-      </div>
-
-      <div class="area">
-        <div class="title border-topbottom">C</div>
-        <div class="item-list">
-          <div class="item border-bottom">CCCC</div>
-          <div class="item border-bottom">CCCC</div>
-          <div class="item border-bottom">CCCC</div>
-        </div>
-      </div>
-
-      <div class="area">
-        <div class="title border-topbottom">D</div>
-        <div class="item-list">
-          <div class="item border-bottom">DDDD</div>
-          <div class="item border-bottom">DDDD</div>
-          <div class="item border-bottom">DDDD</div>
+          <div class="item border-bottom" v-for="innerItem in item" :key="innerItem.id">
+            {{innerItem.name}}
+          </div>
         </div>
       </div>
     </div>
@@ -134,6 +37,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
